e435f622a191191d8dbe66e99e5496bfeb2634b9
romulo garofalo <romulogarofalo@gmail.com>
2020-10-07 09:26:47 +0000

    :tada: created project in phoenix with flags no-html and no-web-pack

---
+++ lib/api_banking/application.ex
@@ -0,0 +1,34 @@
+defmodule ApiBanking.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    children = [
+      # Start the Ecto repository
+      ApiBanking.Repo,
+      # Start the Telemetry supervisor
+      ApiBankingWeb.Telemetry,
+      # Start the PubSub system
+      {Phoenix.PubSub, name: ApiBanking.PubSub},
+      # Start the Endpoint (http/https)
+      ApiBankingWeb.Endpoint
+      # Start a worker by calling: ApiBanking.Worker.start_link(arg)
+      # {ApiBanking.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: ApiBanking.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  # Tell Phoenix to update the endpoint configuration
+  # whenever the application is updated.
+  def config_change(changed, _new, removed) do
+    ApiBankingWeb.Endpoint.config_change(changed, removed)
+    :ok
+  end
+end
