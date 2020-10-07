ab82528a817927af37c06061b4249ac018b704fe
romulo garofalo <romulogarofalo@gmail.com>
2020-10-07 12:07:38 +0000

    :sparkles: facade of user

--- lib/api_banking.ex
+++ lib/api_banking.ex
@@ -1,9 +1,7 @@
 defmodule ApiBanking do
-  @moduledoc """
-  ApiBanking keeps the contexts that define your domain
-  and business logic.
 
-  Contexts are also responsible for managing your data, regardless
-  if it comes from the database, an external API or others.
-  """
+  alias ApiBanking.User
+
+  defdelegate create_user(params), to: User.Create, as: :call
+
 end
