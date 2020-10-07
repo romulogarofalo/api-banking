529c00449d12a8489f6b8e13a1b95b2c9a2cd2a8
romulo garofalo <romulogarofalo@gmail.com>
2020-10-07 12:11:23 +0000

    :sparkles: view of create user

---
+++ lib/api_banking_web/views/user_view.ex
@@ -0,0 +1,34 @@
+defmodule ApiBankingWeb.UserView do
+  use ApiBankingWeb, :view
+
+  alias ApiBanking.User
+
+  def render("created.json", %{
+      user: %User{
+        id: id,
+        user: user,
+        inserted_at: inserted_at
+      },
+      token: token
+    }) do
+    %{
+      message: "User Created",
+      user: %{
+        id: id,
+        user: user,
+        inserted_at: inserted_at
+      },
+      token: token
+    }
+  end
+
+  # If you want to customize a particular status code
+  # for a certain format, you may uncomment below.
+  # def render("500.json", _assigns) do
+  #   %{errors: %{detail: "Internal Server Error"}}
+  # end
+
+  # By default, Phoenix returns the status message from
+  # the template name. For example, "404.json" becomes
+  # "Not Found".
+end
