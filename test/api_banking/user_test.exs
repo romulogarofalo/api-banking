602ee15e7ce1263bc4e7d7c9f0697b51fdac9628
romulo garofalo <romulogarofalo@gmail.com>
2020-10-08 08:10:01 +0000

    :white_check_mark: tests of user schema

---
+++ test/api_banking/user_test.exs
@@ -0,0 +1,55 @@
+defmodule ApiBanking do
+  use ApiBanking.DataCase
+
+  alias ApiBanking.User
+
+  describe "changeset/1" do
+    test "when all params are valid, returns a valid changeset" do
+      params = %{username: "romulo", password: "123123"}
+
+      response = User.changeset(%User{}, params)
+
+      assert %Ecto.Changeset{
+               changes: %{
+                 password: "123123",
+                 username: "romulo"
+               },
+               errors: [],
+               data: %ApiBanking.User{},
+               valid?: true
+             } = response
+    end
+
+    test "when username is  invalid, returns an invalid changeset" do
+      params = %{username: "", password: "123123"}
+
+      response = User.changeset(%User{}, params)
+
+      assert %Ecto.Changeset{
+               changes: %{
+                 password: "123123"
+               },
+               data: %ApiBanking.User{},
+               valid?: false
+             } = response
+
+      assert errors_on(response) == %{username: ["can't be blank"]}
+    end
+
+    test "when password is  invalid, returns an invalid changeset" do
+      params = %{username: "romulo", password: ""}
+
+      response = User.changeset(%User{}, params)
+
+      assert %Ecto.Changeset{
+               changes: %{
+                username: "romulo"
+               },
+               data: %ApiBanking.User{},
+               valid?: false
+             } = response
+
+      assert errors_on(response) == %{password: ["can't be blank"]}
+    end
+  end
+end
