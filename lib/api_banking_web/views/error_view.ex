d8b4ba2d46e29d0b99d4be36ad31d705ee498873
romulo garofalo <romulogarofalo@gmail.com>
2020-10-08 06:24:18 +0000

    :goal_net: addd view of error handler

--- lib/api_banking_web/views/error_view.ex
+++ lib/api_banking_web/views/error_view.ex
@@ -1,6 +1,8 @@
 defmodule ApiBankingWeb.ErrorView do
   use ApiBankingWeb, :view
 
+  import Ecto.Changeset, only: [traverse_errors: 2]
+
   # If you want to customize a particular status code
   # for a certain format, you may uncomment below.
   # def render("500.json", _assigns) do
@@ -10,7 +12,10 @@ defmodule ApiBankingWeb.ErrorView do
   # By default, Phoenix returns the status message from
   # the template name. For example, "404.json" becomes
   # "Not Found".
+
+
   def template_not_found(template, _assigns) do
     %{errors: %{detail: Phoenix.Controller.status_message_from_template(template)}}
   end
+
 end
