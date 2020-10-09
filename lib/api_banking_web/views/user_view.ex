588e9de46701b1e7c1ec8ca269f0c1bb2ea7a6a3
romulo garofalo <romulogarofalo@gmail.com>
2020-10-09 19:45:42 +0000

    :sparkles: added view of login response

--- lib/api_banking_web/views/user_view.ex
+++ lib/api_banking_web/views/user_view.ex
@@ -4,19 +4,33 @@ defmodule ApiBankingWeb.UserView do
   alias ApiBanking.User
 
   def render("created.json", %{
-      user: %User{
-        id: id,
-        username: user,
-        inserted_at: inserted_at
-      },
-      token: token
-    }) do
+        user: %User{
+          id: id,
+          username: user,
+          inserted_at: inserted_at
+        },
+        token: token
+      }) do
     %{
       message: "User Created",
       user: %{
         id: id,
         user: user,
         inserted_at: inserted_at
+      },
+      token: token
+    }
+  end
+
+  def render("authenticated.json", %{
+        user: %User{
+          username: username
+        },
+        token: token
+      }) do
+    %{
+      user: %{
+        username: username
       },
       token: token
     }
