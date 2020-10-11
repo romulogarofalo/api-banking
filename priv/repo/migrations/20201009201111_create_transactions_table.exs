c88fdcbbefcceb440edc14fe1ee1845eb3ab395e
romulo garofalo <romulogarofalo@gmail.com>
2020-10-11 07:23:38 +0000

    :recycle: changed name of fields on migration

--- priv/repo/migrations/20201009201111_create_transactions_table.exs
+++ priv/repo/migrations/20201009201111_create_transactions_table.exs
@@ -3,8 +3,8 @@ defmodule ApiBanking.Repo.Migrations.CreateTransactionsTable do
 
   def change do
     create table(:transactions) do
-      add :username_sender, references(:users)
-      add :username_reciever, references(:users)
+      add :username_sender_id, references(:users)
+      add :username_reciever_id, references(:users)
       add :amount, :integer
       timestamps()
     end
