c00c6a0078ab8eea1bc6f6372c5f55d142870488
romulo garofalo <romulogarofalo@gmail.com>
2020-10-11 08:49:30 +0000

    :sparkles: added facede of create transaction

--- lib/api_banking.ex
+++ lib/api_banking.ex
@@ -1,6 +1,7 @@
 defmodule ApiBanking do
-  alias ApiBanking.User
+  alias ApiBanking.{User, Transaction}
 
   defdelegate create_user(params), to: User.Create, as: :call
   defdelegate get_user(params), to: User.Get, as: :call
+  defdelegate create_transaction(params, sender_id), to: Transaction.Create, as: :call
 end
