1955f587443fb4f7075f2defc28028099c1043fb
romulo garofalo <romulogarofalo@gmail.com>
2020-10-12 02:27:17 +0000

    :bug: fixed bug on pipeline when try to get the user with token

--- lib/api_banking_web/auth/guardian.ex
+++ lib/api_banking_web/auth/guardian.ex
@@ -1,12 +1,13 @@
 defmodule ApiBankingWeb.Auth.Guardian do
   use Guardian, otp_app: :api_banking
+  alias ApiBanking.{User, Repo}
 
   def subject_for_token(user, _claims) do
     sub = to_string(user.id)
     {:ok, sub}
   end
 
-  def resource_from_claims(_claims) do
-    {:error, :reason_for_error}
+  def resource_from_claims(%{"sub" => sub}) do
+    {:ok, Repo.get!(User, sub)}
   end
 end
