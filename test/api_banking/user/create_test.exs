ed78f6dc6bf36f381f701b5a9f4d7d656c6e14c7
romulo garofalo <romulogarofalo@gmail.com>
2020-10-18 08:44:54 +0000

    :white_check_mark: user test of repeated user

--- test/api_banking/user/create_test.exs
+++ test/api_banking/user/create_test.exs
@@ -1,4 +1,4 @@
-defmodule ApiBanking.CreateTest do
+defmodule ApiBanking.User.CreateTest do
   use ApiBanking.DataCase
 
   alias ApiBanking.{Repo, User}
@@ -6,7 +6,7 @@ defmodule ApiBanking.CreateTest do
 
   describe "call/1" do
     test "when all params are valid, creates a trainer" do
-      params = %{username: "romulo", password: "123123"}
+      params = %{username: "romulo", password: "123123", permission: "admin"}
 
       count_before = Repo.aggregate(User, :count)
 
@@ -15,6 +15,27 @@ defmodule ApiBanking.CreateTest do
       count_after = Repo.aggregate(User, :count)
 
       assert {:ok, %User{username: "romulo"}} = response
+      assert count_after > count_before
+    end
+
+    test "when username is repeated" do
+      params = %{username: "romulo", password: "123123", permission: "admin"}
+
+      count_before = Repo.aggregate(User, :count)
+      Create.call(params)
+      response = Create.call(params)
+
+      count_after = Repo.aggregate(User, :count)
+
+      assert {:error,
+              %{
+                errors: [
+                  username:
+                    {"has already been taken",
+                     [constraint: :unique, constraint_name: "users_username_index"]}
+                ]
+              }} = response
+
       assert count_after > count_before
     end
   end
