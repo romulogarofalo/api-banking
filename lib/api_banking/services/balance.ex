b9ccdfcef2e3cb5b6aead95b7f0e3d6a24a8a808
romulo garofalo <romulogarofalo@gmail.com>
2020-10-19 08:47:24 +0000

    :sparkles: added withdraw to get_balance

--- lib/api_banking/services/balance.ex
+++ lib/api_banking/services/balance.ex
@@ -2,14 +2,26 @@ defmodule ApiBanking.Services.Balance do
   alias ApiBanking.Repo
 
   def getBalance(user_id) do
-    cash_out = get_cash_out(user_id)
-    cash_in = get_cash_in(user_id)
+    cash_out_transaction = get_cash_out_transaction(user_id)
+    cash_in_transaction = get_cash_in_transaction(user_id)
     initial_balance = get_initial_balance(user_id)
+    get_cash_out_withdraw = get_cash_out_withdraw(user_id)
 
-    cash_in + initial_balance - cash_out
+    cash_in_transaction + initial_balance - cash_out_transaction - get_cash_out_withdraw
   end
 
-  defp get_cash_out(user_id) do
+  defp get_cash_out_withdraw(user_id) do
+    %{rows: rows_cash_out} =
+      Ecto.Adapters.SQL.query!(
+        Repo,
+        "SELECT amount FROM withdraw WHERE user_id = #{user_id}",
+        []
+      )
+
+    Enum.reduce(rows_cash_out, 0, fn xs, acumulator -> Enum.at(xs, 0) + acumulator end)
+  end
+
+  defp get_cash_out_transaction(user_id) do
     %{rows: rows_cash_out} =
       Ecto.Adapters.SQL.query!(
         Repo,
@@ -17,10 +29,10 @@ defmodule ApiBanking.Services.Balance do
         []
       )
 
-    Enum.reduce(rows_cash_out, 0, fn xs, acumulador -> Enum.at(xs, 0) + acumulador end)
+    Enum.reduce(rows_cash_out, 0, fn xs, acumulator -> Enum.at(xs, 0) + acumulator end)
   end
 
-  defp get_cash_in(user_id) do
+  defp get_cash_in_transaction(user_id) do
     %{rows: rows_cash_in} =
       Ecto.Adapters.SQL.query!(
         Repo,
@@ -28,7 +40,7 @@ defmodule ApiBanking.Services.Balance do
         []
       )
 
-    Enum.reduce(rows_cash_in, 0, fn xs, acumulador -> Enum.at(xs, 0) + acumulador end)
+    Enum.reduce(rows_cash_in, 0, fn xs, acumulator -> Enum.at(xs, 0) + acumulator end)
   end
 
   defp get_initial_balance(user_id) do
