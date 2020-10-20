b966f2d667cec289fd73b4523539ef8b14f68901
romulo garofalo <romulogarofalo@gmail.com>
2020-10-20 21:44:08 +0000

    :sparkles: migration of reports

---
+++ priv/repo/migrations/20201019153246_create_reports_table.exs
@@ -0,0 +1,11 @@
+defmodule ApiBanking.Repo.Migrations.CreateReportsTable do
+  use Ecto.Migration
+
+  def change do
+    create table(:reports) do
+      add :total_amount, :integer
+      add :total_transactions, :integer
+      timestamps()
+    end
+  end
+endre
