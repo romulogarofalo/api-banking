e82865d47dd5fb772137bee7da53deaebfd7eddd
romulo garofalo <romulogarofalo@gmail.com>
2020-10-20 21:58:02 +0000

    :art: changed name of varibles

--- lib/api_banking/scheduler.ex
+++ lib/api_banking/scheduler.ex
@@ -3,15 +3,15 @@ defmodule ApiBanking.Scheduler do
   alias ApiBanking.Repo
 
   def test do
-    %{rows: rows_cash_out} =
+    %{rows: rows} =
       Ecto.Adapters.SQL.query!(
         Repo,
         "SELECT count(*), sum(amount + 0) FROM transactions WHERE inserted_at BETWEEN NOW() and NOW() - INTERVAL '1 DAY';",
         []
       )
 
-    count_transactions = Enum.at(Enum.at(rows_cash_out, 0), 0) || 0
-    sum_transactions = Enum.at(Enum.at(rows_cash_out, 0), 1) || 0
+    count_transactions = Enum.at(Enum.at(rows, 0), 0) || 0
+    sum_transactions = Enum.at(Enum.at(rows, 0), 1) || 0
 
     IO.inspect("geting reports")
 
