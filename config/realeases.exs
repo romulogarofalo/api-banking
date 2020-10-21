9e73c0438caf1402de1716d2e875d15bedd4fef9
romulo garofalo <romulogarofalo@gmail.com>
2020-10-21 10:19:28 +0000

    :rocket: Configure for Gigalixir

---
+++ config/realeases.exs
@@ -0,0 +1,8 @@
+# config/releases.exs
+
+import Config
+
+config :pipsqueak, PipsqueakWeb.Endpoint,
+  server: true,
+  http: [port: {:system, "PORT"}],
+  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443]
